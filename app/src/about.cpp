5c458664a6df4fb4e04fb5ef9745e4a61651ad6d
Ömer Göktaş <omer@omergoktas.com>
2017-02-08 03:43:16 +0300

    Update about

--- app/src/about.cpp
+++ app/src/about.cpp
@@ -7,7 +7,7 @@
 #include <fit.h>
 #include <QApplication>
 
-#define TITLE_TEXT "<p><b>version</b> 1.58 <b>pbuild</b> 03b17dd<br>Wed Feb 8 01:16:28 2017 +0300<br></p>"
+#define TITLE_TEXT "<p><b>version</b> 1.581 <b>pbuild</b> 558be4e<br>Wed Feb 8 03:42:02 2017 +0300<br></p>"
 #define LEGAL_TEXT "<p><b>© 2015-2017 Objectwheel, Inc. All Rights Reserved.</b></p>"
 
 using namespace Fit;
