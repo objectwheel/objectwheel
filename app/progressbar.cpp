4821a47dffc40261e6080ce91bad867be105f13a
Ömer Göktaş <omer@omergoktas.com>
2018-07-14 05:29:10 +0300

    template4.zip and template5.zip are fixed progressbar.cpp indeterminateColor improved previewer bug fix

--- app/progressbar.cpp
+++ app/progressbar.cpp
@@ -22,7 +22,7 @@ ProgressBar::ProgressBar(QWidget *parent) : QProgressBar(parent)
     m_settings.backgroundColor = "#12000000";
     m_settings.chunkColor = "#419BF9";
     m_settings.borderColor = "#40000000";
-    m_settings.indeterminateColor = "#50ffffff";
+    m_settings.indeterminateColor = "#45ffffff";
     m_settings.borderRadius = SIZE.height() / 2.0;
 
     setIndeterminate(true);
