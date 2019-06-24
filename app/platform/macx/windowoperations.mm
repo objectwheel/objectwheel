3af987379cc40a39a94061227eff5a5f33bca36e
Ömer Göktaş <omer@omergoktas.com>
2019-06-24 08:08:25 +0300

    WindowOperations::removeTitleBar is improved in order to fix bad dragging bug on the tool bar.
    Signed-off-by: Ömer Göktaş <omer@omergoktas.com>

--- app/platform/macx/windowoperations.mm
+++ app/platform/macx/windowoperations.mm
@@ -9,14 +9,52 @@ namespace WindowOperations {
 void removeTitleBar(QMainWindow* mainWindow)
 {
     mainWindow->setUnifiedTitleAndToolBarOnMac(true);
+    mainWindow->setWindowFlags(mainWindow->windowFlags() & ~Qt::WindowFullscreenButtonHint);
+
     auto nsView = (NSView*) mainWindow->winId();
     auto window = [nsView window];
-    auto toolBar = [[NSToolbar alloc] initWithIdentifier:@"ScreenNameToolbarIdentifier"];
-    toolBar.showsBaselineSeparator = false;
-    [window setToolbar: toolBar];
-    [window setTitleVisibility: NSWindowTitleHidden];
+    auto close = [window standardWindowButton: NSWindowCloseButton];
+    auto minimize = [window standardWindowButton: NSWindowMiniaturizeButton];
+    auto maximize = [window standardWindowButton: NSWindowZoomButton];
+    auto container = close.superview;
+
     window.movable = false;
+    window.titlebarAppearsTransparent = true;
     window.styleMask |= NSWindowStyleMaskFullSizeContentView;
+    [window setTitleVisibility: NSWindowTitleHidden];
+
+    container.translatesAutoresizingMaskIntoConstraints = false;
+    [container.superview willRemoveSubview: container];
+    [container removeFromSuperview];
+    [container viewWillMoveToSuperview: window.contentView];
+    [window.contentView addSubview: container];
+    [container viewDidMoveToSuperview];
+
+    close.translatesAutoresizingMaskIntoConstraints = false;
+    [close.widthAnchor constraintEqualToConstant: 14].active = true;
+    [close.heightAnchor constraintEqualToConstant: 14].active = true;
+    [close.leadingAnchor constraintEqualToAnchor: container.leadingAnchor constant: 0.0].active = true;
+    [close.centerYAnchor constraintEqualToAnchor: container.centerYAnchor].active = true;
+
+    minimize.translatesAutoresizingMaskIntoConstraints = false;
+    [minimize.widthAnchor constraintEqualToConstant: 14].active = true;
+    [minimize.heightAnchor constraintEqualToConstant: 14].active = true;
+    [minimize.leadingAnchor constraintEqualToAnchor: close.trailingAnchor constant: 6.0].active = true;
+    [minimize.centerYAnchor constraintEqualToAnchor: close.centerYAnchor].active = true;
+
+    maximize.translatesAutoresizingMaskIntoConstraints = false;
+    [maximize.widthAnchor constraintEqualToConstant: 14].active = true;
+    [maximize.heightAnchor constraintEqualToConstant: 14].active = true;
+    [maximize.leadingAnchor constraintEqualToAnchor: minimize.trailingAnchor constant: 6.0].active = true;
+    [maximize.centerYAnchor constraintEqualToAnchor: minimize.centerYAnchor].active = true;
+
+    [container.widthAnchor constraintEqualToConstant: 54].active = true;
+    [container.heightAnchor constraintEqualToConstant: 14].active = true;
+    [container.topAnchor constraintEqualToAnchor: window.contentView.topAnchor constant: 12.0].active = true;
+    [container.leadingAnchor constraintEqualToAnchor: window.contentView.leadingAnchor constant: 10.0].active = true;
+
+    [window.contentView layoutSubtreeIfNeeded];
+    [window.contentView.superview viewDidEndLiveResize];
 }
 
 }
